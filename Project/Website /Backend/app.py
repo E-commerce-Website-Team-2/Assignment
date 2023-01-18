4d6345e66c2bd87932392d13690eee2531c65ed8
GouravAravinda <93828276+GouravAravinda@users.noreply.github.com>
2023-01-18 15:42:41 +0530

    Feat (Data Ingestion) + Feat(Backend)
    The data ingestion API can now be used to add to the category tree as well. The backend has a skeleton code added to it now.

--- Project/Website /Backend/app.py
+++ Project/Website /Backend/app.py
@@ -1,13 +1,34 @@
-from flask import Flask ,request, jsonify
+from flask import Flask ,request, jsonify, request
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'This is my first API call!'
+@app.route('/products/search', methods=["GET"])
+def query():
+    searchquery = request.args.get('query')
+    unbxdsearchAPI = "https://search.unbxd.io/fb853e3332f2645fac9d71dc63e09ec1/demo-unbxd700181503576558/search?q="
+    finalquery = unbxdsearchAPI + searchquery
+    responseFromSearch = request.get(finalquery).content
+    
 
-@app.route('/post', methods=["POST"])
-def testpost():
-     input_json = request.get_json(force=True) 
-     dictToReturn = {'text':input_json['text']}
-     return jsonify(dictToReturn)
+
+@app.route('/products/category', methods=["GET"])
+def category():
+    pass
+
+
+
+
+@app.route('/products/trending', methods=["GET"])
+def trending():
+    pass
+
+
+@app.route('/products/getcategory', methods = ["GET"])
+def getcategory():
+    pass
+
+
+
+@app.route('/products/details/</product-id>', methods=["GET"])
+def details(productId):
+    pass
