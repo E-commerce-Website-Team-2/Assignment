c87f85427ddc3853d8acd93f49fe9a1dd7629ffa
kuntal0901 <kuntal0901@gmail.com>
2023-01-19 16:35:41 +0530

    refactor(Frontend): CHanges in frontend that dynamically updates the category from the backend response

--- Project/Website /Frontend/script.js
+++ Project/Website /Frontend/script.js
@@ -55,23 +55,46 @@ fetch("http://localhost:5000/products/getcategory",{
 )
   .then((response) => response.json())
   .then((data) => {
-    // const category1 = document.getElementById("myDropdown1");
-    // var subcategory = document.createElement("a");
-    // subcategory.setAttribute("href", "#");
-    // subcategory.innerHTML = "Subcategory1";
-    // category1.appendChild(subcategory);
-    
-    // var subcategory2 = document.createElement("a");
-    // subcategory2.setAttribute("href", "#");
-    // subcategory2.innerHTML = "Subcategory2";
-    // category1.appendChild(subcategory2);
-    console.log(data);
+      for (var subcategory in data.men){
+        var subcategory1 = document.createElement("a");
+        subcategory1.setAttribute("href", "#");
+        subcategory1.innerHTML = data.men[subcategory];
+        document.getElementById("myDropdown1").appendChild(subcategory1);
+      }
+      for (var subcategory in data.women){
+        var subcategory2 = document.createElement("a");
+        subcategory2.setAttribute("href", "#");
+        subcategory2.innerHTML = data.women[subcategory];
+        document.getElementById("myDropdown2").appendChild(subcategory2);
+      }
+    console.log(data.men[0]);
   })
   .catch((error) => {
     console.log(error);
   });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 //   var xhr = null;
 //   getXmlHttpRequestObject = function () {
 //       if (!xhr) {
