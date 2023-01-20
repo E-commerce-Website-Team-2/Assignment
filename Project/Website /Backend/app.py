1f8f3a2528b1a24ac589b2bb0478f923a900e1ef
GouravAravinda <gourav.aravinda@gmail.com>
2023-01-20 10:16:08 +0530

    fix(Backend): Changed the parameters that the search API will return

--- Project/Website /Backend/app.py
+++ Project/Website /Backend/app.py
@@ -74,7 +74,7 @@ def query():
     start = 0   #(pagenumber - 1)*9
     rows = 9
     unbxdsearchAPI = "https://search.unbxd.io/fb853e3332f2645fac9d71dc63e09ec1/demo-unbxd700181503576558/search?q="
-    finalquery = unbxdsearchAPI + searchquery + "&start=" + str(start) + "&rows=" + str(rows) + "&fields=" + "uniqueId,price,productDescription,productImage"
+    finalquery = unbxdsearchAPI + searchquery + "&start=" + str(start) + "&rows=" + str(rows) + "&fields=" + "uniqueId,name,price,productImage"
     responseFromSearch = (requests.get(finalquery).content)
     responseFromSearch = json.loads(responseFromSearch)
     #Pagination will be carried out by using a page number as a variable in the URL 
