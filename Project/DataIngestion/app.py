cbfac7379b0e2c69bb90748a5f8eb6c3ae7d567d
GouravAravinda <gourav.aravinda@gmail.com>
2023-01-31 12:55:38 +0530

    feat(Backend-+-Redis): BAckend has now been implemented in conjunction with the redis cache and deployed on docker

--- Project/DataIngestion/app.py
+++ Project/DataIngestion/app.py
@@ -31,6 +31,7 @@ def settrendingcaller():
     return settrending()
 
 
-app.run()
+
+app.run(debug=True, host="0.0.0.0", port=6000)
 
 
--- Project/Website/Backend/Database/DBmain.py
+++ Project/Website/Backend/Database/DBmain.py
@@ -3,7 +3,7 @@ import psycopg2
 #This function will be able to create a connection to the database. It will be called everytime a connection 
 # is asked for and should return a connection to the database as and when requested. 
 def db_connection(db_name):
-    conn = psycopg2.connect(host='localhost',
+    conn = psycopg2.connect(host='project-database-1',
                             database=db_name,
                             user='gourav' ,    #os.environ['DB_USERNAME'],
                             password='gourav')         #os.environ['DB_PASSWORD'])
